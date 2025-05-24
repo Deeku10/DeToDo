c4ea3e0d36743d7855e9733ebce5557a4af1e886
Deeku10 <96645036+Deeku10@users.noreply.github.com>
2025-05-24 08:03:46 +0530

    first commit of detodo_springboot

---
+++ DeToDo_springboot/src/main/java/in/builtbydeeku/detodo_springboot/DeToDoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package in.builtbydeeku.detodo_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeToDoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeToDoSpringbootApplication.class, args);
+	}
+
+}
---
+++ DeToDo_springboot/src/test/java/in/builtbydeeku/detodo_springboot/DeToDoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package in.builtbydeeku.detodo_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeToDoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
